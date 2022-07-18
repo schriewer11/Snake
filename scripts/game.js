493baf80996003cb4b1d7b9906587ee974024194
schriewer11 <schriewer11@yahoo.com>
2022-07-18 11:41:06 -0400

    created instance of phaser, added images for apple and snake, created Scene1

---
+++ scripts/game.js
@@ -0,0 +1,9 @@
+
+let config = {
+    width: 800,
+    height: 600,
+    backgroundColor: 0x000000,
+    autoCenter: true,
+ }
+let game = new Phaser.Game( config );
+
---
+++ scripts/scenes/Scene1.js
@@ -0,0 +1,14 @@
+class Scene1 extends Phaser.Scene {
+
+    // this will boot up the game
+    constructor() {
+        super( 'bootGame' );
+    }
+
+
+    // load all of the images and animations for the game
+    preload() {
+        
+    }
+
+}
